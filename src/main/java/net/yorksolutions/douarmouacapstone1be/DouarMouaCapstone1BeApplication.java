66bf94f5250a2b7980957cc5f216c96fa6806013
douar <mouadouarzong@gmail.com>
2022-11-15 10:18:17 -0600

    Set up database

---
+++ src/main/java/net/yorksolutions/douarmouacapstone1be/DouarMouaCapstone1BeApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.douarmouacapstone1be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DouarMouaCapstone1BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DouarMouaCapstone1BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yorksolutions/douarmouacapstone1be/DouarMouaCapstone1BeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.douarmouacapstone1be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DouarMouaCapstone1BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
