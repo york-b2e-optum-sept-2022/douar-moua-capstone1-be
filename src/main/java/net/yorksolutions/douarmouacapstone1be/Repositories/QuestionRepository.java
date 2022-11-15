9eaaf78909e766e026a099a448096ebf568452bb
douar <mouadouarzong@gmail.com>
2022-11-15 10:55:19 -0600

    Rename question repo to QuestionRepository Create Question service and inject QuestionRepository

--- src/main/java/net/yorksolutions/douarmouacapstone1be/Repositories/QuestionRepository.java
+++ src/main/java/net/yorksolutions/douarmouacapstone1be/Repositories/QuestionRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Question extends CrudRepository<Question, Long> {
+public interface QuestionRepository extends CrudRepository<QuestionRepository, Long> {
 }
