142e1f610a47905e1bb0154e7deb52c07ff3254d
douar <mouadouarzong@gmail.com>
2022-11-15 10:57:38 -0600

    Delete question repo and question service

--- src/main/java/net/yorksolutions/douarmouacapstone1be/Repositories/QuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package net.yorksolutions.douarmouacapstone1be.Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends CrudRepository<QuestionRepository, Long> {
-}
