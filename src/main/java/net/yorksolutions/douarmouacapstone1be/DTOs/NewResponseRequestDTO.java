d6130c54e8d08bb96a696fd5d99074b57549813f
douar <mouadouarzong@gmail.com>
2022-11-28 23:37:26 -0600

    New Response DTO

---
+++ src/main/java/net/yorksolutions/douarmouacapstone1be/DTOs/NewResponseRequestDTO.java
@@ -0,0 +1,9 @@
+package net.yorksolutions.douarmouacapstone1be.DTOs;
+
+public class NewResponseRequestDTO {
+    public String answer;
+    public Long surveyId;
+    public String surveyTitle;
+    public Long questionId;
+    public String question;
+}
